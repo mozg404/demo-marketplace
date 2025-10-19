8b52c374172ee7a1e07b447ed5113a82cd6fbb3c
mozg404 <masterdmx3325@gmail.com>
2025-10-19 21:07:20 +0500

    feat: require redis for cache and queue

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Contracts\Cart;
 use App\Models\Category;
 use App\Services\Cart\CartService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 use Inertia\Inertia;
 
 class TestController extends Controller
@@ -15,10 +16,14 @@ class TestController extends Controller
 
     public function test(CartService $cart): mixed
     {
-//        $cart->add(1,2);
-//        $cart->add(2,1);
+        // Тест кеширования
+        Cache::put('test_key', 'Hello Redis!', 60);
+        $value = Cache::get('test_key');
 
-        return $cart->all();
+        return [
+            'cache' => $value,
+            'redis_connection' => Redis::ping(),
+        ];
     }
 
 
