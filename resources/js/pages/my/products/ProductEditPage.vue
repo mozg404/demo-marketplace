3b6d3f3e07eeb676d48e6a1b60d897dbd96f1928
mozg404 <masterdmx3325@gmail.com>
2025-10-19 20:01:39 +0500

    fix: avatar icon in edit page + product statuses

--- resources/js/pages/my/products/ProductEditPage.vue
+++ resources/js/pages/my/products/ProductEditPage.vue
@@ -200,7 +200,7 @@ const props = defineProps({
 
           <Card class="py-4 mb-4">
             <CardContent class="px-4 flex items-center">
-              <UserAvatarIcon class="mr-3" :src="product.user.avatar_url"/>
+              <UserAvatarIcon class="mr-3" :user="product.user"/>
               <div>
                 <div class="text-muted-foreground text-xs mb-[-4px]">Продавец</div>
                 <Link :href="route('users.show', product.user.id)" class="font-semibold text-sm hover:text-primary">{{product.user.name}}</Link>
